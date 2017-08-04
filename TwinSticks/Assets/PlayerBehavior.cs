6d22b7d8a7291474afde19ba1958ac220bd93993
Dominic Lagle <Dominic.Lagle444@gmail.com>
2017-08-04 14:07:39 -0700

    01_Commit Unity Twin Sticks Test Scene Using CrossPlatformInputManager

---
+++ TwinSticks/Assets/PlayerBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerBehavior : MonoBehaviour {
+    
+	void Start ()
+    {	
+	}
+	
+	void Update ()
+    {
+        Debug.Log("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+        Debug.Log("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+    }
+}
